28f83340addfa7b885ea2043c87d89e4fc9b712d
dima <balecored@gmail.com>
2026-03-11 03:38:59 +0500

    Разработанное окно MainWindow. Шаповалов.

--- 22-24/MainWindow.xaml.cs
+++ 22-24/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +21,37 @@ namespace _22_24
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static Connection connect;
+        public static Pages.Main main;
         public MainWindow()
         {
             InitializeComponent();
+            connect = new Connection();
+            connect.LoadData(Connection.tabels.users);
+            connect.LoadData(Connection.tabels.calls);
+
+            main = new Pages.Main();
+            OpenPageMain();
+        }
+
+        public void OpenPageMain()
+        {
+            DoubleAnimation opgridAnimation = new DoubleAnimation();
+            opgridAnimation.From = 1;
+            opgridAnimation.To = 0;
+            opgridAnimation.Duration = TimeSpan.FromSeconds(0.6);
+            opgridAnimation.Completed += delegate
+            {
+                frame.Navigate(main);
+
+                DoubleAnimation opgrisAnimation = new DoubleAnimation();
+                opgrisAnimation.From = 0;
+                opgrisAnimation.To = 1;
+                opgrisAnimation.Duration = TimeSpan.FromSeconds(1.2);
+
+                frame.BeginAnimation(Frame.OpacityProperty, opgrisAnimation);
+            };
+            frame.BeginAnimation(Frame.OpacityProperty, opgridAnimation);
         }
     }
 }
